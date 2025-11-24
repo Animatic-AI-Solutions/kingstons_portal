03db4398982e8e4a5c53667332f41bba7cd285f5
Jacob <jacob.b.mcnulty.2019@gmail.com>
2025-11-24 15:03:53 +0000

    added product report link

--- frontend/src/pages/ProductIRRCalculation.tsx
+++ frontend/src/pages/ProductIRRCalculation.tsx
@@ -1837,7 +1837,24 @@ const AccountIRRCalculation: React.FC<AccountIRRCalculationProps> = ({ accountId
               </span>
             </div>
           )}
-          <PresenceIndicator 
+          {/* Generate Report Button */}
+          <button
+            onClick={() => {
+              navigate('/report-generator', {
+                state: {
+                  preSelectedClientGroupId: account?.client_id,
+                  preSelectedClientGroupName: account?.client_name
+                }
+              });
+            }}
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-primary-600 border border-transparent rounded-lg hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 transition-all duration-200 shadow-sm"
+          >
+            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            Generate Report
+          </button>
+          <PresenceIndicator
             pageIdentifier={`irr-calculation:product-${accountId}`}
             className="flex-shrink-0"
           />
--- prompt.md
+++ prompt.md
@@ -1,7 +1,1 @@
- now i got reefback from the client that instead fo having 2 fee types (fixed and percentage) theree shoudl now be 3 types (fixed fee      
-direct, fixed fee facilitated, percentage fee facilitated), so we will have to mmodify the the create clienbt products page, the product    
- overview page, the lcient gropup detaisl page, the revenue page and possibly the databse structure as well\
-\
-Lets first start by renamining fixed fee to 'fixed fee facilitated', thsi is the fiurst and most simple step so lets start here
-
-First make a plan to refactor fixed fee to fixed fee faciliated (only fixed fee and not percentage fee) this includes changing names of some views and tables
+in the product irr calculation page have a button which takes the user to the report generator page with the client group of the product populated in the client client gourp dropdown, use the generta report button in the lciebt details page for guidance, this button should take the user to the geenrator page with the lcient group already populated, 
